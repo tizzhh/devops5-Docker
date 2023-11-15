e1178b193010257206132c7a2bb5649e2563f309
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-15 18:30:12 +0300

    part6.

--- part_5/build.sh
+++ part_5/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# export DOCKER_CONTENT_TRUST=1
 docker build -t fastcgi_nginx:1.0 -f Dockerfile-part5 .
 docker run -d -p 80:81 --name server -v $(pwd)/nginx:/etc/nginx fastcgi_nginx:1.0 
